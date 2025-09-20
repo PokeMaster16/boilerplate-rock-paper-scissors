36ea633a040225dad9154b439ed6e02af3ba3b52
PokeMaster16 <pandemania55.@gmail.com>
2025-09-20 23:58:46 +0100

    Update RPS.py
    Create a bot that play rock, paper, scissors

--- RPS.py
+++ RPS.py
@@ -3,8 +3,24 @@
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    #guess = "R"
+    #if len(opponent_history) > 2:
+    #    guess = opponent_history[-2]
 
-    return guess
+    #return guess
+if not opponent_history:
+        return "R"
+
+    # Count how often opponent uses each move
+    from collections import Counter
+    counts = Counter(opponent_history)
+    most_common = counts.most_common(1)[0][0]
+
+    # Counter the most common move
+    if most_common == "R":
+        return "P"
+    elif most_common == "P":
+        return "S"
+    else:
+        return "R"
+
